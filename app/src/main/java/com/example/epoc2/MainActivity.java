22f8c49d320239ed43374b93624efcb2549d6d1b
HectorVillavicencio <103118349+HectorVillavicencio@users.noreply.github.com>
2023-09-18 00:44:15 -0300

    icono y agregue un medidor

--- app/src/main/java/com/example/epoc2/MainActivity.java
+++ app/src/main/java/com/example/epoc2/MainActivity.java
@@ -2,19 +2,25 @@ package com.example.epoc2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.ekn.gruzer.gaugelibrary.HalfGauge;
+import com.ekn.gruzer.gaugelibrary.Range;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText textNroCig, textNroAnio;
     private TextView textView, textView2;
-
+    private HalfGauge idMedidor;
+    private com.ekn.gruzer.gaugelibrary.Range Range1, Range2, Range3, Range4;
+    private int SetearGrafica;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +34,34 @@ public class MainActivity extends AppCompatActivity {
         //icono en el action bar
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         //getSupportActionBar().setIcon(R.mipmap.ic_launcher); agrega el icono al lado de la letra. Para tenerlo
+        //aca empiezo ahcer el medidor
+        idMedidor = findViewById(R.id.idMedidor);
+
+        Range1 = new com.ekn.gruzer.gaugelibrary.Range();
+        Range2 = new com.ekn.gruzer.gaugelibrary.Range();
+        Range3 = new com.ekn.gruzer.gaugelibrary.Range();
+        Range4 = new com.ekn.gruzer.gaugelibrary.Range();
+
+        Range1.setFrom(0); Range1.setTo(10);
+        Range2.setFrom(11); Range2.setTo(20);
+        Range3.setFrom(21); Range3.setTo(40);
+        Range4.setFrom(41); Range4.setTo(50);
+
+        Range1.setColor(Color.GREEN);
+        Range2.setColor(Color.YELLOW);
+        Range3.setColor(Color.parseColor("#FFA500"));
+        Range4.setColor(Color.RED);
+
+        idMedidor.setValue(0);
+        idMedidor.setMinValue(0);
+        idMedidor.setMaxValue(50);
+
+        idMedidor.addRange(Range1);
+        idMedidor.addRange(Range2);
+        idMedidor.addRange(Range3);
+        idMedidor.addRange(Range4);
+
+
     }
 
     //Verifica si estan todos los valores, si no lanza una alerta
@@ -36,8 +70,8 @@ public class MainActivity extends AppCompatActivity {
         String anio = textNroAnio.getText().toString();
 
         //notificacion de error
-        this.tieneNull(cig, "Dale, deci cuanto fumas por dia capo");
-        this.tieneNull(anio, "Deci hace cuanto fumas, porque no te hago un carajo así");
+        this.tieneNull(cig, "Falta la cantidad de cigarrillos diarios");
+        this.tieneNull(anio, "Falta la cantidad de años que fumas");
 
         this.calculaSiTieneTodosLosDatos(cig,anio);
 
@@ -48,6 +82,7 @@ public class MainActivity extends AppCompatActivity {
         if(cig.length() != 0 && anio.length() !=0){
             this.calcular();
             this.frase();
+            idMedidor.setValue(calcula(Integer.parseInt(cig),Integer.parseInt(anio)));
         }
     }
 
@@ -78,13 +113,13 @@ public class MainActivity extends AppCompatActivity {
         int valor2 = Integer.parseInt(textNroAnio.getText().toString());
         // Verificar el rango del resultado y mostrar el mensaje apropiado
         if (this.calcula(valor1 ,valor2) < 10) {
-            textView2.setText(String.valueOf("¡Bien! Tu salud es importante :)"));
+            textView2.setText(String.valueOf("¡Bien!)"));
         } else if (calcula(valor1 ,valor2) >= 10 && calcula(valor1 ,valor2) < 20) {
-            textView2.setText(String.valueOf("Elevado. Empeza a bajar los cigarrillos."));
+            textView2.setText(String.valueOf("Elevado"));
         } else if (calcula(valor1 ,valor2) >= 21 && calcula(valor1 ,valor2) <= 40) {
-            textView2.setText(String.valueOf("Peligroso. Encerio, estas jodido, reduci el consumo."));
+            textView2.setText(String.valueOf("Peligroso"));
         } else {
-            textView2.setText(String.valueOf("Muy mal. Anda aprendiendo a tocar el arpa o baja el consumo"));
+            textView2.setText(String.valueOf("Muy peligroso"));
         }
     }
 
