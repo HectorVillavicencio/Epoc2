8208fd46da9afaa52a856a7e351472f07db022e3
HectorVillavicencio <fvillavicencio2@gmail.com>
2024-09-18 08:36:14 -0300

    cambio para probar git

--- app/src/main/java/com/example/epoc2/evaluator/CalculatorCigarretesAndAnios.java
+++ app/src/main/java/com/example/epoc2/evaluator/CalculatorCigarretesAndAnios.java
@@ -5,7 +5,7 @@ public class CalculatorCigarretesAndAnios {
     //Resuelve el calculo
     public float calcula(int v1, int v2){
 
-        return (v1 * v2) / 20;
+        return (v1 * v2) / 10;
     }
 
 
